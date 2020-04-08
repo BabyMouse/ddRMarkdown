a97f201756431311adb10e2d0a53dc9987bd9112
BabyMouse <babymouse011@gmail.com>
2020-04-09 01:06:51 +0700

    Add new file.

---
+++ _template/sourceembed.R
@@ -0,0 +1,23 @@
+# Source: https://github.com/yihui/xfun/blob/master/R/markdown.R
+# xfun::embed_file('ddrIntroduction.Rmd')
+xfun::pkg_load2(c("base64enc", "htmltools", "mime"))
+embed_file_a <- function(
+                         file,
+                         name = basename(file),
+                         text = paste("Download", name),
+                         ...) {
+  h <- paste0(
+    "data:",
+    mime::guess_type(file),
+    ";base64,",
+    base64enc::base64encode(file)
+  )
+  htmltools::a(
+    id = "embed-file",
+    href = h,
+    download = name,
+    hidden = NA,
+    text,
+    ...
+  )
+}
